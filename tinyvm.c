44384cb81e8e8c9c3dacd59a41a7aea57533799b
Joseph Kogut <joseph.kogut@gmail.com>
2011-01-11 00:52:49 -0700

    * Added tinyvm.c, which was not committed in the last revision.
    * Improved makefile

---
+++ tinyvm.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "virtual_machine.h"
+
+int main(int argc, char** argv)
+{
+	char* filename = "";
+
+	if(argc == 2)
+		filename = argv[1];
+
+	virtual_machine* vm = create_vm(filename);
+	if(vm != NULL) run_vm(vm);
+
+	// Print the first memory address
+	if(vm) printf("%i\n", vm->pMemory->int32[0]);
+
+	destroy_vm(vm);
+
+	return 0;
+}
