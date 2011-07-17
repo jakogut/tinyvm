f86b6d149d0f05eb9b425ddd523299cab5e54420
Joseph Kogut <joseph.kogut@gmail.com>
2011-07-17 14:38:09 -0700

    Changed instruction and register indices from decimal to hexadecimal

--- tvm_instr_idx.h
+++ tvm_instr_idx.h
@@ -2,56 +2,56 @@
 #define INSTRUCTION_INDEX_H_
 
 // End of the program
-#define END -1
+#define END -0x1
 
 // Comment
-#define COM 0
+#define COM 0x0
 
 // Interrupt
-#define INT 1
+#define INT 0x1
 
 // Move a value to an address
-#define MOV 2
+#define MOV 0x2
 
 // Stack instructions
-#define PUSH 3
-#define POP 4
+#define PUSH 0x3
+#define POP 0x4
 
 // Increment/Decrement
-#define INC 5
-#define DEC 6
+#define INC 0x5
+#define DEC 0x6
 
 // Arithmetic
-#define ADD 7
-#define SUB 8
-#define MUL 9
-#define DIV 10	// Divide, storing the quotient in the first argument
-#define MOD 11	// Divide, storing the remainder in the remainder register
-#define REM 12	// Copy the remainder register to the address specified by the first argument
+#define ADD 0x7
+#define SUB 0x8
+#define MUL 0x9
+#define DIV 0xA	// Divide, storing the quotient in the first argument
+#define MOD 0xB	// Divide, storing the remainder in the remainder register
+#define REM 0xC	// Copy the remainder register to the address specified by the first argument
 
 // Bitwise instructions
-#define NOT 13
-#define XOR 14
-#define OR  15
-#define AND 16
+#define NOT 0xD
+#define XOR 0xE
+#define OR  0xF
+#define AND 0x10
 
 // Shifts
-#define SHL 17
-#define SHR 18
+#define SHL 0x11
+#define SHR 0x12
 
 // Compare
-#define CMP 19
+#define CMP 0x13
 
 // Conditionals
-#define JMP 20
+#define JMP 0x14
 
-#define JE  21
-#define JNE 22
+#define JE  0x15
+#define JNE 0x16
 
-#define JG 23
-#define JGE 24
+#define JG 0x17
+#define JGE 0x18
 
-#define JL 25
-#define JLE 26
+#define JL 0x19
+#define JLE 0x1A
 
 #endif
--- tvm_reg_idx.h
+++ tvm_reg_idx.h
@@ -1,17 +1,17 @@
 #ifndef TVM_REG_IDX_H_
 #define TVM_REG_IDX_H_
 
-#define AX 0
-#define BX 1
-#define CX 2
-#define DX 3
+#define AX 0x0
+#define BX 0x1
+#define CX 0x2
+#define DX 0x3
 
-#define SI 4
-#define DI 5
+#define SI 0x4
+#define DI 0x5
 
-#define SP 6
-#define BP 7
+#define SP 0x6
+#define BP 0x7
 
-#define IP 8
+#define IP 0x8
 
 #endif
