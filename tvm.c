d1f9840393d4984fab325054dc27ca4ccf991117
Joseph Kogut <joseph.kogut@gmail.com>
2011-08-13 03:13:49 -0700

    * Added jump to subroutine functionality
    * Added NOP instruction
    
    * Added program demonstrating the usage of the "call" and "ret" instructions
    
    * Added program demonstrating the usafe of the "nop" instruction

--- tvm.c
+++ tvm.c
@@ -33,34 +33,38 @@ void run_vm(tvm_t* vm)
 
 		switch(vm->pProgram->instr[*instr_idx])
 		{
-			case 0x1:  *arg0 = *arg1; break;
-			case 0x2:  stack_push(vm->pMemory, arg0); break;
-			case 0x3:  stack_pop(vm->pMemory, arg0); break;
-			case 0x4:  stack_push(vm->pMemory, &vm->pMemory->FLAGS); break;
-			case 0x5:  stack_pop(vm->pMemory, arg0); break;
-			case 0x6:  ++(*arg0); break;
-			case 0x7:  --(*arg0); break;
-			case 0x8:  *arg0 += *arg1; break;
-			case 0x9:  *arg0 -= *arg1; break;
-			case 0xA:  *arg0 *= *arg1; break;
-			case 0xB:  *arg0 /= *arg1; break;
-			case 0xC:  vm->pMemory->remainder = *arg0 % *arg1; break;
-			case 0xD:  *arg0 = vm->pMemory->remainder; break;
-			case 0xE:  *arg0 = ~(*arg0); break;
-			case 0xF:  *arg0 ^= *arg1;   break;
-			case 0x10: *arg0 |= *arg1;   break;
-			case 0x11: *arg0 &= *arg1;   break;
-			case 0x12: *arg0 <<= *arg1;  break;
-			case 0x13: *arg0 >>= *arg1;  break;
-			case 0x14: vm->pMemory->FLAGS = ((*arg0 == *arg1) | (*arg0 > *arg1) << 1); break;
-			case 0x15: *instr_idx = *arg0 - 1; break;
-			case 0x16: if(vm->pMemory->FLAGS   & 0x1)  *instr_idx = *arg0 - 1; break;
-			case 0x17: if(!(vm->pMemory->FLAGS & 0x1)) *instr_idx = *arg0 - 1; break;
-			case 0x18: if(vm->pMemory->FLAGS   & 0x2)  *instr_idx = *arg0 - 1; break;
-			case 0x19: if(vm->pMemory->FLAGS   & 0x3)  *instr_idx = *arg0 - 1; break;
-			case 0x1A: if(!(vm->pMemory->FLAGS & 0x3)) *instr_idx = *arg0 - 1; break;
-			case 0x1B: if(!(vm->pMemory->FLAGS & 0x2)) *instr_idx = *arg0 - 1; break;
-			case 0x1C: printf("%i\n", *arg0);
+			case 0x0:  break;
+			case 0x1:
+			case 0x2:  *arg0 = *arg1; break;
+			case 0x3:  stack_push(vm->pMemory, arg0); break;
+			case 0x4:  stack_pop(vm->pMemory, arg0); break;
+			case 0x5:  stack_push(vm->pMemory, &vm->pMemory->FLAGS); break;
+			case 0x6:  stack_pop(vm->pMemory, arg0); break;
+			case 0x7:  ++(*arg0); break;
+			case 0x8:  --(*arg0); break;
+			case 0x9:  *arg0 += *arg1; break;
+			case 0xA:  *arg0 -= *arg1; break;
+			case 0xB:  *arg0 *= *arg1; break;
+			case 0xC:  *arg0 /= *arg1; break;
+			case 0xD:  vm->pMemory->remainder = *arg0 % *arg1; break;
+			case 0xE:  *arg0 = vm->pMemory->remainder; break;
+			case 0xF:  *arg0 = ~(*arg0); break;
+			case 0x10:  *arg0 ^= *arg1;  break;
+			case 0x11: *arg0 |= *arg1;   break;
+			case 0x12: *arg0 &= *arg1;   break;
+			case 0x13: *arg0 <<= *arg1;  break;
+			case 0x14: *arg0 >>= *arg1;  break;
+			case 0x15: vm->pMemory->FLAGS = ((*arg0 == *arg1) | (*arg0 > *arg1) << 1); break;
+			case 0x17: stack_push(vm->pMemory, instr_idx);
+			case 0x16: *instr_idx = *arg0 - 1; break;
+			case 0x18: stack_pop(vm->pMemory, instr_idx);
+			case 0x19: if(vm->pMemory->FLAGS   & 0x1)  *instr_idx = *arg0 - 1; break;
+			case 0x1A: if(!(vm->pMemory->FLAGS & 0x1)) *instr_idx = *arg0 - 1; break;
+			case 0x1B: if(vm->pMemory->FLAGS   & 0x2)  *instr_idx = *arg0 - 1; break;
+			case 0x1C: if(vm->pMemory->FLAGS   & 0x3)  *instr_idx = *arg0 - 1; break;
+			case 0x1D: if(!(vm->pMemory->FLAGS & 0x3)) *instr_idx = *arg0 - 1; break;
+			case 0x1E: if(!(vm->pMemory->FLAGS & 0x2)) *instr_idx = *arg0 - 1; break;
+			case 0x1F: printf("%i\n", *arg0);
 		};
 	}
 }
