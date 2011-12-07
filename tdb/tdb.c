d6879ced1139545d72581150f1b5c67206ae625f
Bl0ckeduser <bl0ckedusersoft@gmail.com>
2011-12-07 15:55:22 -0500

    Improve, debug, document tdb

--- tdb/tdb.c
+++ tdb/tdb.c
@@ -7,7 +7,7 @@
 #define MAX_TOKENS 3
 #define MAX_INPUT_LENGTH 256
 
-const char* commands[] = {"q", "run", "break", "step", 0};
+const char* commands[] = {"q", "run", "break", "step", "continue", 0};
 
 static void tokenize(char* str, char** tokens);
 static int cmd_to_idx(char* cmd);
@@ -15,6 +15,7 @@ static int cmd_to_idx(char* cmd);
 void tdb_shell(tvm_t* vm)
 {
 	int run = 1, i;
+	int running = 0;
 
 	char* tokens[MAX_TOKENS];
 	char str[MAX_INPUT_LENGTH];
@@ -26,7 +27,7 @@ void tdb_shell(tvm_t* vm)
 
         vm->pMemory->registers[0x8].i32 = vm->pProgram->start;
 
-	while(run)
+	while(run && !feof(stdin))
 	{
 		int retcode = 0;
 
@@ -39,19 +40,22 @@ void tdb_shell(tvm_t* vm)
 
 		switch(cmd_to_idx(tokens[0]))
 		{
-			case -0x1: printf("\nWARNING: \"%s\" is not a valid command.\n", tokens[0]); break;
+			case -0x1: printf("WARNING: \"%s\" is not a valid command.\n", tokens[0]); break;
 			case  0x0: run = 0; break;
-			case  0x1: retcode = tdb_run(vm, breakpoints, num_breakpoints); break;
+			case  0x1: if(running) printf("The program is already running.\n");
+				   else { retcode = tdb_run(vm, breakpoints, num_breakpoints); running = 1; } break;
 			case  0x2: breakpoints = realloc(breakpoints, sizeof(tdb_breakpoint_t) * ++num_breakpoints);
 				   breakpoints[num_breakpoints - 1].address = tvm_parse_value(tokens[1]); break;
 			case  0x3: tvm_step(vm, &vm->pMemory->registers[0x8].i32);
 				   vm->pMemory->registers[0x8].i32++;
 				   printf("Advancing instruction pointer to %i\n", vm->pMemory->registers[0x8].i32); break;
+			case  0x4: tvm_step(vm, &vm->pMemory->registers[0x8].i32);
+				   vm->pMemory->registers[0x8].i32++;
+				   retcode = tdb_run(vm, breakpoints, num_breakpoints); break;
 		};
 
+		if(vm->pProgram->instr[vm->pMemory->registers[0x8].i32] == -0x1) printf("End of program reached\n");
 		if(retcode == 1) printf("Breakpoint hit at address: %i\n", vm->pMemory->registers[0x8].i32);
-
-		printf("\n");
 	}
 
 	free(breakpoints);
@@ -70,7 +74,6 @@ int tdb_run(tvm_t* vm, tdb_breakpoint_t* breakpoints, int num_breakpoints)
 			if(breakpoints[i].address == *instr_idx) return 1; /* Breakpoint hit */
 
 		tvm_step(vm, instr_idx);
-
 	}
 
 	return 0;
