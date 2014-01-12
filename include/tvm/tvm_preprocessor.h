fc0e1d31848e07c6621c37ca3786be580a328357
Payton Turnage <paytonturnage@gmail.com>
2014-01-12 11:33:50 -0500

    Make program objects track defines.
    Program objects will now track defines. The htab for defines will be passed
    to the parser module in future committs, which will scan and replace any
    instances of the defined string.
    
    Possible modifications in future commits:
    * Do not use an htab for defines; rather a list of structs with a key/val
    * Use the integer value allocated for htab elements as a pointer to the
      value string instead of an integer.

--- include/tvm/tvm_preprocessor.h
+++ include/tvm/tvm_preprocessor.h
@@ -1,6 +1,8 @@
 #ifndef TVM_PREPROCESSOR_H_
 #define TVM_PREPROCESSOR_H_
 
-int tvm_preprocess(char *src, int *src_len);
+#include "tvm_htab.h"
+
+int tvm_preprocess(char *src, int *src_len, tvm_htab_t *htab);
 
 #endif
--- libtvm/tvm_preprocessor.c
+++ libtvm/tvm_preprocessor.c
@@ -3,7 +3,7 @@
 
 #include <string.h>
 
-int tvm_preprocess(char *src, int *src_len)
+int tvm_preprocess(char *src, int *src_len, tvm_htab_t *htab)
 {
 	char* pp_directive_delimiter = NULL;
 	if((pp_directive_delimiter = strstr(src, "%include")))
@@ -46,6 +46,45 @@ int tvm_preprocess(char *src, int *src_len)
 		for(int i = 0; i < new_src_len; i++) if(src[i] == 0) src[i] = ' ';
 
 		*src_len = strlen(src);
+		return 1;
+	}
+	else if ((pp_directive_delimiter = strstr(src, "%define")))
+	{
+		char *strbegin = pp_directive_delimiter, *strend = strchr(strbegin, '\n');
+
+		if (!strend || !strbegin) return 0;
+
+		char *define = (strchr(strbegin, ' ') + 1);
+		char *valstr = define ? (strchr(define, ' ') + 1) : NULL;
+
+		//Limit searching for value string to define's line
+		if (!define || !valstr || (valstr > strend)) {
+			*strend = 0;
+			printf("Define missing arguments: \"%s\"\n", strbegin);
+			*strend = '\n';
+			return 0;
+		}
+
+		*strend = '\n';
+
+		char *tmp = valstr - 1;
+		*tmp = 0;
+
+		int value = atoi(valstr);
+
+		htab_add(htab, define, value);
+
+		size_t first_block_len = (strbegin - src);
+		size_t second_block_len = ((src + *src_len) - strend);
+		size_t new_src_len = (first_block_len + second_block_len);
+
+		memcpy(&src[first_block_len], strend, second_block_len);
+
+		src = (char *)realloc((char *)src, sizeof(char) * new_src_len);
+		src[new_src_len] = 0;
+
+		*src_len = new_src_len;
+
 		return 1;
 	}
 
